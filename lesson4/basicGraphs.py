b591a5a3b151470ee8c4b1b6435e7b199229c5e9
Sergey Bliacharskiy <sergey16032000@gmail.com>
2023-02-13 14:31:18 +0200

    load data to show

--- lesson4/basicGraphs.py
+++ lesson4/basicGraphs.py
@@ -2,45 +2,41 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def loadPrices(fileName):
-    data = pd.read_csv(fileName)
-    data = data.set_index(pd.DatetimeIndex(data['Date']))
-    data = data.drop('Date', 1)
+    data = pd.read_csv(fileName, sep=",", header=None)
+    data.columns = ["Date", "Open", "High", "Low", 'Price', 'Volume', 'DateEnd']
     return data
 
-#price@t0 = 100%
-def normalizeValues(table,newColumn,existingColumn):
-    priceAtT0 = table.iloc[-1][existingColumn]
-    table[newColumn] = table.apply(lambda row: (row[existingColumn]/priceAtT0), axis = 1)
+
+# price@t0 = 100%
+def normalizeValues(table, newColumn, existingColumn):
+    priceAtT0 = table.iloc[+1][existingColumn]
+    table[newColumn] = table.apply(lambda row: (row[existingColumn] / priceAtT0), axis=1)
 
     return table
 
 
-# oil = loadPrices('../data/oilPrices.csv')
-# usdRubPrice = loadPrices('../data/usdPrices.csv')
-#
-# #re-index index USD = index Oil
-# commonIndex = oil.index
-# usdRubPrice = usdRubPrice.reindex(commonIndex)
-# usdRubPrice = usdRubPrice.fillna(method='backfill')
-#
-# #normalizing values
-# usdRubPrice = normalizeValues(usdRubPrice,'normalizedValue','Price')
-# oil = normalizeValues(oil,'normalizedValue','Price')
-#
-#
-#
-# Xaxis = commonIndex
-# # #Y = price
-# OilYaxis = oil['normalizedValue'].values
-# RubYAxis = usdRubPrice['normalizedValue'].values
-#
-# #
-# #
-# #
-# plt.plot(Xaxis,OilYaxis,color="red", label='Oil Price')
-# plt.plot(Xaxis,RubYAxis, 'b--', label='Цена рублика')
-# plt.legend()
-# plt.show()
+btc = loadPrices('../data/BTCUSDT_01-01-2022_29-01-2023.txt')
+eth = loadPrices('../data/ETHUSDT_01-01-2022_29-01-2023.txt')
+
+#re-index index USD = index Oil
+commonIndex = btc.index
+
+#normalizing values
+eth = normalizeValues(eth, 'normalizedValue', 'Price')
+btc = normalizeValues(btc, 'normalizedValue', 'Price')
 
 
+Xaxis = commonIndex
+# #Y = price
+btcYaxis = btc['normalizedValue'].values
+ethYAxis = eth['normalizedValue'].values
+
+#
+#
+#
+plt.plot(Xaxis, btcYaxis, color="red", label='BTC')
+plt.plot(Xaxis, ethYAxis, 'b--', label='ETH')
+plt.legend()
+plt.show()
