c6050db7df8132671ee91c5ec0bd8f1e1bd00c1d
Sergey Bliacharskiy <sergey16032000@gmail.com>
2023-04-03 22:43:56 +0300

    plot task finished

--- plot_task.py
+++ plot_task.py
@@ -1,9 +1,11 @@
 import numpy as np
 from matplotlib import pyplot as plt
-
-x = np.random.randint(2,size=(100,2))
-y = np.logical_xor(x[:,0], x[:,1])
-
+x = np.random.random((2000, 2))
+# x = np.random.randint(2,size=(100,2))
+# y = np.logical_xor(x[:,0], x[:,1])
+# print(f)
+y = np.logical_xor(np.around(x[:,0]), np.around(x[:,1]))
+# print(h)
 print(x)
 print(y)
 plt.scatter(x[:,0], x[:,1], c=y)
